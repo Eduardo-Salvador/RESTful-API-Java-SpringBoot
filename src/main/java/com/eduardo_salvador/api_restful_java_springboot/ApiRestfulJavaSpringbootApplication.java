8ea1d359eeb17c92c8a6dd5f5da870d7efd185e2
Eduardo-Salvador <eduardo.salvador.gs@gmail.com>
2026-03-31 15:31:22 -0300

    build: Converting project to Spring to start the application

---
+++ src/main/java/com/eduardo_salvador/api_restful_java_springboot/ApiRestfulJavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo_salvador.api_restful_java_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulJavaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulJavaSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardo_salvador/api_restful_java_springboot/ApiRestfulJavaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo_salvador.api_restful_java_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulJavaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
