444d65ea46c5be9236ab23b59bc7661bd903eaa4
Eduardo-Salvador <eduardo.salvador.gs@gmail.com>
2026-04-28 16:42:11 -0300

    feat: Containerizing the application with Docker

--- src/test/java/com/eduardo_salvador/api_restful_java_springboot/ProductServiceIntegrationTest.java
+++ src/test/java/com/eduardo_salvador/api_restful_java_springboot/ProductServiceIntegrationTest.java
@@ -9,7 +9,7 @@ import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
